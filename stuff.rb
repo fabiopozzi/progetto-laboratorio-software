9b5b2e1d8f561e65137b25ad31d3dee5ae56bdb7
Fabio Pozzi <pozzi.fabio@gmail.com>
2011-04-01 18:59:27 +0200

    miglioramenti nella lettura delle opzioni da file di configurazione e nell'uso degli altri componenti

--- stuff.rb
+++ stuff.rb
@@ -1,10 +1,12 @@
 #!/usr/bin/env ruby
 $LOAD_PATH << './lib'
+require 'rubygems' # usato per poter sfruttare la gemma ruby-elf
 require 'function_hook.rb'
 require 'pp'
+require 'elf'
 
 module Stuff
-  FILENAME = './options'
+  OPTIONS_FILE = './options'
   def Stuff.init_wrappers(library_name)
     # Returns an hash containing the code to be inserted in each wrapper function.
     # Author:: Fabio Pozzi (mailto:pozzi.fabio@gmail.com)
@@ -21,28 +23,93 @@ module Stuff
   end
 
   def Stuff.get_params_from_process
-	h_hash = [{ :function => 'ctest1'},{:function => 'ctest2'}]
-	#h_hash = [{ :function => 'ctest2'}]
-	# run our Hook engine on a running 'notepad' instance
-	l = LibraryHook.new('prog', h_hash)
+  h_hash = [{ :function => 'ctest1'},{:function => 'ctest2'}]
+  #h_hash = [{ :function => 'ctest2'}]
+  # run our Hook engine on a running 'notepad' instance
+  l = LibraryHook.new('prog', h_hash)
         argomenti = l.get_arguments #get infos about wrapped functions
         pp argomenti
         argomenti
   end
 
-  def Stuff.get_library_name
-    File.open(FILENAME).each do |option|
-      if(/LIB/ ~= option) # o qualcosa di simile
-        parts = option.split(/\t/)
-        library_name = parts[1] # LIB=<tab>library_name is the configuration schema
-        return library_name
+  # Il metodo apre il file contenente tutti i punti di interesse dello stato
+  # # e cerca un match, restituendo un array con tutti i risultati del grep
+  def Stuff.file_grep( search_string)
+    results = Array.new
+    #puts search_string
+    File.open( OPTIONS_FILE ) do |fh|
+      fh.grep( /(.*)#{search_string}(.*)/ ) do |line|
+        results << line
       end
     end
+    results
   end
+
+  def Stuff.get_library_name
+    raw_line = Stuff.file_grep("LIB")
+    line = raw_line.first.chomp
+    #pp raw_line
+
+    name_array = line.split(/ /)   #LIB=<tab>library_name is the configuration schema
+    #puts name_array[1]
+    return name_array[1] # the name is the 2nd element, the one after the token separator
+  end
+
   def Stuff.get_functions_list
     # Retrieves a list of functions to be wrapped by our code
+    list = Array.new
+    raw_list = Stuff.file_grep("FUNCTION")
+    raw_list.each do |line|
+      line = line.chomp # removes trailing whitespace and '\n'
+      el = Hash.new
+      name_array = line.split(/ /)
+      el[:function]=name_array[1]
+      list << el
+    end
+    list
   end
+
+  def Stuff.get_arguments(libname)
+    base_path = "../"
+    libpath = base_path + libname
+    begin
+      section = '.dynsym'
+      Elf::File.open( libpath ) do |elf|
+      addrsize = (elf.elf_class == Elf::Class::Elf32 ? 8 : 16)
+
+      if not elf.has_section? section
+        $stderr.puts " #{elf.path} is not a dynamic library"
+        exitval = 1
+        next
+      end
+      symbols = Array.new
+      elf[section].each do |sym|
+        next if sym.name == ''
+        begin
+          flag = sym.nm_code
+          rescue Elf::Symbol::UnknownNMCode => e
+            $stderr.puts e.message
+          end
+          next if sym.nm_code != 'T'
+          next if sym.name == "_init"
+          next if sym.name == "_fini"
+          puts "#{sym.address_string} #{flag} #{sym.name}#{sym.version}"
+          symbols << sym
+        end
+      end
+    rescue Errno::ENOENT
+      $stderr.puts " {file}: No such file"
+    end
+    symbols
+  end # end of get_arguments
+
 end
 
 #Stuff.get_params_from_process
-Stuff.get_library_name
+libname = Stuff.get_library_name
+wrapme = Stuff.get_functions_list
+pp wrapme
+Generators.generate_hooks(libname)
+l = LibraryHook.new('prog', wrapme)
+argomenti = l.get_arguments #get infos about wrapped functions
+pp argomenti
